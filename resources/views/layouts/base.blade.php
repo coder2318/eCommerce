c7cc9cffe92be4e397aedafaa08f13a9deb3754a
Farrukh <choriyevfarrux997@gmail.com>
2021-09-16 20:55:12 +0500

    change logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -117,7 +117,7 @@
                 <div class="mid-section main-info-area">
 
                     <div class="wrap-logo-top left-section">
-                        <a href="index.html" class="link-to-home"><img src="{{asset('assets/images/logo-top-1.png')}}" alt="mercado"></a>
+                        <a href="{{route('home')}}" class="link-to-home"><img src="{{asset('assets/images/onshopuz.png')}}" alt="mercado" width="40%"></a>
                     </div>
 
                     @livewire('search-header-component')
